894d5fb4e35f3608d07515f48dd0dcf491ab1fa5
elisemkbell <69221533+elisemkbell@users.noreply.github.com>
2020-08-05 13:01:59 -0500

    uploaded all files

---
+++ main.js
@@ -0,0 +1,6 @@
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+var video1 = document.getElementBySrc("Dwalk1.mp4");
+var video2 = document.getElementBySrc("Dwalk2.mp4");
+var video3 = document.getElementBySrc("Dwalk3.mp4");
